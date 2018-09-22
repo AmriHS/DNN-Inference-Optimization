17360f265bff04b3aafd98fff19144e5bc97938a
Hassan Alamri <alamri.cs@gmail.com>
2018-09-21 22:01:37 -0400

    Refactoring
    Refactoring

--- Keras_Resnet50.py
+++ Keras_Resnet50.py
@@ -11,7 +11,6 @@ import numpy as np
 import cv2 #, os
 import csv
 
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
 #imgs = []
 #for file in os.listdir("C://Users//hcaro//OneDrive//Documents//images"):
@@ -35,31 +34,27 @@ def resize(dataset):
         processed_data.append(preprocess_input(x))
     return processed_data
 
-num_cores = 8
-config = tf.ConfigProto(intra_op_parallelism_threads=8,
+
+def make_predictions(dataset, num_cores, threads_parallel, batch_size):
+    config = tf.ConfigProto(intra_op_parallelism_threads=8,
                         inter_op_parallelism_threads=8, allow_soft_placement=True, log_device_placement=False,
                         device_count = {'CPU' : num_cores, 'GPU' : 0}) #cpu_memory=3.75
-session = tf.Session(config=config)
-ktf.set_session(session)
+    session = tf.Session(config=config)
+    ktf.set_session(session)
 
-model = ResNet50(weights='imagenet')
+    model = ResNet50(weights='imagenet')
+    # start time
+    start = start = timer()
+    preds = model.predict(np.array(dataset), batch_size=batch_size)
+    # end time
+    end = timer()
+    # calculate runtime
+    elapsed_time_batch = end-start
+    print('Runtime: ' + "{0:.2f}".format(end-start) + 's')
+    return preds
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
 dataset = resize(X_train[:200])
-
-# start time
-start = start = timer()
-batchsize = 128
-predictions = []
-pred = model.predict(np.array(dataset), batch_size=batchsize)
-predictions.append(pred)
-
-# end time
-end = timer()
-# calculate runtime
-elapsed_time_batch = end-start
-print('Runtime: ' + "{0:.2f}".format(end-start) + 's')
-
-decoded_preds = []
-for pred in predictions:
-    decoded =  decode_predictions(pred)
-    decoded_preds.append(decoded)
-write_to_csv(decoded_preds, "result2.csv")
+preds = make_predictions(dataset, num_cores=4, threads_parallel=16, batch_size=32)
+decoded =  decode_predictions(preds, top=1)
+write_to_csv(decoded, "result1.csv")
