21c5f21c72e5a3e837e88e78136ad04ea59e77c9
Hassan Alamri <alamri.cs@gmail.com>
2018-11-27 23:09:25 -0500

    Update
    Sampling Data
    Sampling code

---
+++ src code/SVR_prediction.py
@@ -0,0 +1,153 @@
+from sklearn.datasets import make_regression
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.svm import SVR
+from sklearn.pipeline import Pipeline
+import numpy as np
+import pandas as pd
+from math import sqrt
+from sklearn.metrics import mean_squared_error
+from matplotlib import pyplot
+import matplotlib.pyplot as plt
+from sklearn import cross_validation
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+
+def polynomial(data_x, data_y, split, init_degree=2, max_degree = 2):
+    #data_x = np.array(data_x, dtype="float64")
+    #data_y = np.array(data_y, dtype="float64")
+    #data_y = np.round(data_y, decimals=1)
+
+    X_train, Y_train = data_x[:split], data_y[:split]
+    X_test, Y_test = data_x[split:], data_y[split:]
+
+    degrees = np.arange(init_degree,max_degree+1)
+    pred = []
+    for i in range(len(degrees)):
+        model = Pipeline([
+            ('poly', PolynomialFeatures(degree=degrees[i])),
+            ('linreg', LinearRegression(normalize=True))
+        ])
+        model.fit(X_train, Y_train)
+        Y_train_pred = model.predict(X_train)
+        Y_test_pred = model.predict(X_test)
+        pred.append(Y_test_pred)
+
+        intercept = model.named_steps['linreg'].intercept_[0]
+        coef = model.named_steps['linreg'].coef_[0]
+        features = model.named_steps['poly'].get_feature_names()
+        assert(len(coef) == len(features))
+
+        estimated_inf_f = '{:+.2f}'.format(intercept)
+
+        for j in range(0, len(coef)-1):
+            if float ('{:+.1f}'.format(coef[j]).replace('\U00002013', '-')) != 0.0:
+                estimated_inf_f += ' + {:+.1f} {}'.format(np.round(coef[j], decimals=2), features[j+1])
+
+        print (estimated_inf_f)
+        # plot function we want to learn
+        rmse_infer = sqrt(mean_squared_error(Y_test_pred[:,0], Y_test[:,0]))
+        rmse_power = sqrt(mean_squared_error(Y_test_pred[:,1], Y_test[:,1]))
+
+        print('Test RMSE for Inference Time: %.3f' % rmse_infer)
+        print('Test RMSE for Power Consumption: %.3f' % rmse_power)
+    return pred
+
+def test_multi_target_regression(data_x, data_y):
+    n_train = int (len(data_x)*0.80)
+    X_train, Y_train = data_x[:n_train], data_y[:n_train]
+    X_test, Y_test = data_x[n_train:], data_y[n_train:]
+
+    rgr = MultiOutputRegressor(GradientBoostingRegressor(random_state=0))
+    rgr.fit(X_train, Y_train)
+    Y_train_pred = rgr.predict(X_train)
+    Y_test_pred = rgr.predict(X_test)
+
+    plot_result(Y_train[:,0],Y_test[:,0], Y_train_pred[:,0], Y_test_pred[:,0])
+    plot_result(Y_train[:,1],Y_test[:,1], Y_train_pred[:,1], Y_test_pred[:,1])
+
+    rmse_infer = sqrt(mean_squared_error(Y_test_pred[:,0], Y_test[:,0]))
+    rmse_power = sqrt(mean_squared_error(Y_test_pred[:,1], Y_test[:,1]))
+    rmse_train_infer = sqrt(mean_squared_error(Y_train_pred[:,0], Y_train[:,0]))
+    rmse_train_power = sqrt(mean_squared_error(Y_train_pred[:,1], Y_train[:,1]))
+
+    print('Train RMSE for Inference Time: %.3f' % rmse_train_infer)
+    print('Train RMSE for Power Consumption: %.3f' % rmse_train_power)
+    print('Test RMSE for Inference Time: %.3f' % rmse_infer)
+    print('Test RMSE for Power Consumption: %.3f' % rmse_power)
+
+
+def plot_min_iteration(bay_y, rand_y):
+    assert (len(bay_y) == len(rand_y))
+    plt.plot(np.arange(0, bay_y.shape[0]),np.minimum.accumulate(bay_y[:,0]) ,'darkslateblue',label='Baysian Opt Inference')
+    plt.plot(np.arange(0, rand_y.shape[0]),np.minimum.accumulate(rand_y[:,0]) ,'darkseagreen',label='Random Inference')
+    plt.ylabel('fmin')
+    plt.xlabel('Number of evaluated points')
+    plt.legend()
+    plt.show()
+
+    plt.plot(np.arange(0, bay_y.shape[0]),np.minimum.accumulate(bay_y[:,1]) ,'firebrick',label='Baysian Opt Power Consumption')
+    plt.plot(np.arange(0, rand_y.shape[0]),np.minimum.accumulate(rand_y[:,1]) ,'royalblue',label='Random Power Consumption')
+    plt.ylabel('fmin')
+    plt.xlabel('Number of evaluated points')
+    plt.legend()
+    plt.show()
+
+def plot_result(Y_train,Y_test, Y_train_pred, Y_test_pred):
+    data_y = np.concatenate((Y_train, Y_test), axis=0)
+    data_y = data_y.reshape((data_y.shape[0], 1))
+    sequence_arr = np.arange(1,len(data_y)+1).reshape(len(data_y),1)
+
+    pyplot.plot(sequence_arr,data_y, 'o', color='firebrick', label='ground truth')
+    pyplot.plot(sequence_arr[:len(Y_train)],Y_train_pred,'cornflowerblue',label='pred-train')
+    pyplot.plot(sequence_arr[len(Y_train):],Y_test_pred,'darkslategray',label='pred-test')
+    pyplot.legend(loc='best')
+    pyplot.show()
+
+dir_path = 'C:/Users/hcaro/Google Drive/Fall 2018/MLS/Project/DNN_Inference/Experiment Result'
+bays_dataset = pd.read_csv(dir_path+'/Bays_VDD_40.csv')
+rand_dataset = pd.read_csv(dir_path+'/Random_vgg_40.csv') #
+
+# baysian Optimization Dataset
+bays_dataset = bays_dataset.values[:,:-2]
+bays_dataset = bays_dataset.astype('float32')
+
+# random Optimization Dataset
+rand_dataset = rand_dataset.values[:,:-2]
+rand_dataset = rand_dataset.astype('float32')
+
+
+# exclude CPU & GPU consumption
+bays_data_x, bays_data_y = bays_dataset[:,:-2], bays_dataset[:,-2:]
+rand_data_x, rand_data_y = rand_dataset[:,:-2], rand_dataset[:,-2:]
+
+test_multi_target_regression(bays_data_x, bays_data_y)
+#print ("---------------------------------------------")
+test_multi_target_regression(rand_data_x, rand_data_y)
+
+
+#print ("#########################")
+
+sequence_arr = np.arange(1,len(bays_data_x)+1).reshape(len(bays_data_x),1)
+n_train = int (len(bays_data_x)*0.8)
+max_degree = 3
+degrees = np.arange(2,max_degree+1)
+pyplot.plot(sequence_arr[n_train:],bays_data_y[n_train:,0],'o', color='navy', linewidth="2", marker='o', label='ground truth')
+pyplot.plot(sequence_arr[n_train:],bays_data_y[n_train:,0], color='cornflowerblue', linewidth="2", label='ground truth')
+pred_list = polynomial(bays_data_x, bays_data_y, n_train, init_degree=3, max_degree=max_degree)
+colors = ["navy", "brown", "teal", "darkslategray"]
+for i in range(len(pred_list)):
+    pyplot.plot(sequence_arr[n_train:],pred_list[i][:,0],color=colors[i],linewidth=2,label='Degree %d' %degrees[i])
+pyplot.legend(loc='best')
+pyplot.show()
+
+pyplot.plot(sequence_arr[n_train:],bays_data_y[n_train:,1],'o', color='navy', linewidth="2", marker='o', label='ground truth')
+pyplot.plot(sequence_arr[n_train:],bays_data_y[n_train:,1], color='cornflowerblue', linewidth="2", label='ground truth')
+colors = ["navy", "brown", "teal", "darkslategray"]
+for i in range(len(pred_list)):
+    pyplot.plot(sequence_arr[n_train:],pred_list[i][:,1],color=colors[i],linewidth=2,label='Degree %d' %degrees[i])
+pyplot.legend(loc='best')
+pyplot.show()
+
+
+plot_min_iteration(bays_data_y, rand_data_y)
--- src code/baysian_opt.py
+++ src code/baysian_opt.py
@@ -25,16 +25,28 @@ command='python run_bench_v2.py --cpu_freq {0} --num_cores {1} --gpu_freq {2} --
 command2='python DNN_Inference/ssh_ex.py'
 
 # define discrete values of each input space
-batch_size_array=[1, 8, 16, 32]
+batch_size_array=[1, 8, 16, 32] # 16 & 32 is excluded for VGG16
 all_growth_array=[True, False]
-memory_frac_array=[0.1, 0.15, 0.2, 0.25, 0.3, 0.33] # 0.1 is exluded due to memory issue
+memory_frac_array=[0.15, 0.2, 0.25, 0.3, 0.33] # 0.15, 0.2,  is excluded for VGG16 model
 GPU_freq_array=[76800000,153600000, 230400000, 307200000, 384000000, 460800000, 537600000, 614400000,
                 691200000, 768000000, 844800000, 921600000, 998400000, 537600000,998400000]
 CPU_freq_array=[102000,204000,306000, 408000, 510000, 612000, 714000, 816000, 918000,1020000,1122000, 1224000,
                 1326000,1224000,1428000,1555000,1632000, 1734000]
+
+# 40800000 frequency excluded due to frequent memory issue
 EMC_freq_array=[800000000,1065600000,1331200000,1600000000]
-# 40800000 frequency excluded due to memory issue
 num_dis_core_array=[0,1,2,3]
+
+# define space input, upper and lower bounds for each dimension
+domain = gpflowopt.domain.ContinuousParameter('CPU_frequency', 102000, 1734000) + \
+         gpflowopt.domain.ContinuousParameter('num_cores_dis', 0, 4) + \
+         gpflowopt.domain.ContinuousParameter('GPU_frequency', 76800000, 998400000) + \
+         gpflowopt.domain.ContinuousParameter('EMC_frequency', 800000000, 1600000000)+ \
+         gpflowopt.domain.ContinuousParameter('Batch_size', 1, 32) + \
+         gpflowopt.domain.ContinuousParameter('Allow_growth', 0, 1) + \
+         gpflowopt.domain.ContinuousParameter('Memory_fraction', 0.15, 0.33)
+
+is_sampling = True
 
 # connect to linux server using ssh
 ssh=paramiko.SSHClient()
@@ -50,23 +62,6 @@ def write_to_csv(data, filename):
         for row in data:
             csv_out.writerow(row)
 
-def vlmop2(x):
-    transl = 1 / np.sqrt(2)
-    part1 = (x[:, [0]] - transl) ** 2 + (x[:, [1]] - transl) ** 2
-    part2 = (x[:, [0]] + transl) ** 2 + (x[:, [1]] + transl) ** 2
-    y1 = 1 - np.exp(-1 * part1)
-    y2 = 1 - np.exp(-1 * part2)
-    return np.hstack((y1, y2))
-
-# round input values
-def round(params):
-    params[:, [0]] = np.round(params[:, [0]]*1000000, decimals=0)
-    params[:, [1]] = np.round(params[:, [1]], decimals=0)
-    params[:, [2]] = np.round(params[:, [2]]*1000000, decimals=0)
-    params[:, [3]] = np.round(params[:, [3]]*1000000000, decimals=0)
-    params[:, [4]] = np.round(params[:, [4]], decimals=2)
-    params[:, [6]] = np.round(params[:, [6]], decimals=2)
-
 # find closest discrete values to the continuous input
 def find_closest(values):
     values[0] = min(CPU_freq_array, key=lambda x:abs(x-values[0]))
@@ -77,24 +72,22 @@ def find_closest(values):
     values[5] = min(all_growth_array, key=lambda x:abs(x-values[5]))
     values[6] = min(memory_frac_array, key=lambda x:abs(x-values[6]))
 
-is_sampling = True
 
 def handle_output(output):
     if len(output) == 0:
-        output = [None, None,None, None]
-    elif not is_sampling and len(output) != 0:
-        output = [float(output[0]), float(output[1]), float(output[2]), float(output[3])]
-    return output
+        return [None, None, None, None]
+    else:
+        return [float(output[0]), float(output[1]), float(output[2]), float(output[3])]
 
 # Optimization multo-objective function
 def objective_func(params):
-    print (params.shape)
     y1 = []
     y2 = []
     benchmark_data = []
     for i in range(len(params)):
         print ("Iteration:"+str(i))
         find_closest(params[i])
+        print (params[i,1])
         cmd = command.format(int(params[i,0]), int(params[i,1]),int(params[i,2]),int(params[i,3]),int(params[i,4]),
                              int(params[i,5]),float(params[i,6]))
         print ("Command:"+repr(cmd))
@@ -104,6 +97,7 @@ def objective_func(params):
         outlines=stdout.readlines()
         response=''.join(outlines).split()
         response = handle_output(response)
+        print (response)
         benchmark_data.append([params[i,0], params[i,1], params[i,2], params[i,3], params[i,4], params[i,5],
                          params[i,6], response[0], response[1], response[2], response[3]])
         y1.append([response[0]])
@@ -111,44 +105,49 @@ def objective_func(params):
     write_to_csv(benchmark_data, "result.csv")
     return np.hstack((y1,y2))
 
-def random_search(n_iter):
-    params = []
-    for i in range(1,n_iter+1):
-        cpu_r = random.randint(102000, 1734000)
-        core_r = random.randint(0, 4)
-        gpu_r = random.randint(76800000, 998400000)
-        emc_r = random.randint(800000000, 1600000000)
-        batch_r = random.randint(1, 32)
-        mem_r = random.randint(0,1)
-        grow_r = random.randint(0.15, 0.33)
-        params.append([cpu_r, core_r, gpu_r, emc_r, batch_r, mem_r, grow_r])
-    return objective_func(params)
-
-def baysian_opt():
-    # define space input, upper and lower bounds for each dimension
-    domain = gpflowopt.domain.ContinuousParameter('CPU_frequency', 102000, 1734000) + \
-             gpflowopt.domain.ContinuousParameter('num_cores_dis', 0, 4) + \
-             gpflowopt.domain.ContinuousParameter('GPU_frequency', 76800000, 998400000) + \
-             gpflowopt.domain.ContinuousParameter('EMC_frequency', 800000000, 1600000000)+ \
-             gpflowopt.domain.ContinuousParameter('Batch_size', 1, 32) + \
-             gpflowopt.domain.ContinuousParameter('Allow_growth', 0, 1) + \
-             gpflowopt.domain.ContinuousParameter('Memory_fraction', 0.15, 0.33)
-# Setup input domain
-    #domain = gpflowopt.domain.ContinuousParameter('x1', -2, 2) + \
-    #     gpflowopt.domain.ContinuousParameter('x2', -2, 2)
-    # EMC frequency -> 0.0408 discarded due to memory issues
-    n_samples = 2
-    design = gpflowopt.design.RandomDesign(n_samples, domain, )
+def random_search():
+    n_samples = 40
+    design = gpflowopt.design.RandomDesign(n_samples, domain)
     X = design.generate()
     Y = objective_func(X)
     itemindex = np.where(Y[:n_samples]==None) #discard samples where it doesn't produce output
+    Y = np.delete(Y, (itemindex[0]), axis=0)
+    Y = np.array(Y, dtype=float)
+
+    plt.scatter(Y[:,0], Y[:,1])
+    plt.title('Random set')
+    plt.xlabel('Inference Time')
+    plt.ylabel('Power Consumption')
+    plt.show()
+
+    print (Y.shape[0])
+
+    plt.plot(np.arange(0, Y.shape[0]),np.minimum.accumulate(Y[:,0]) ,'b',label='Inference Time')
+    plt.ylabel('fmin')
+    plt.xlabel('Number of evaluated points')
+    plt.legend()
+    plt.show()
+
+    plt.plot(np.arange(0, Y.shape[0]),np.minimum.accumulate(Y[:,1]) ,'g',label='Power Consumption')
+    plt.ylabel('fmin')
+    plt.xlabel('Number of evaluated points')
+    plt.legend()
+    plt.show()
+
+def baysian_opt():
+    global is_sampling
+    n_samples = 10
+    design = gpflowopt.design.LatinHyperCube(n_samples, domain)
+    X = design.generate()
+    X = np.array(X)
+    Y = objective_func(X)
+    # discard samples where it doesn't produce output
+    itemindex = np.where(Y[:n_samples]==None)
     Y = np.delete(Y, (itemindex[0]), axis=0)
     X = np.delete(X, (itemindex[0]), axis=0)
     Y = np.array(Y, dtype=float)
     n_samples = len(X)
     is_sampling = False
-    print (X)
-    print (Y)
     # One model for each objective
     objective_models = [gpflow.gpr.GPR(X.copy(), Y[:,[i]].copy(), gpflow.kernels.Matern52(domain.size, ARD=True)) for i in range(Y.shape[1])]
     for model in objective_models:
@@ -158,17 +157,11 @@ def baysian_opt():
     acquisition_opt = gpflowopt.optim.StagedOptimizer([gpflowopt.optim.MCOptimizer(domain, n_samples),
                                                        gpflowopt.optim.SciPyOptimizer(domain)])
 
-# Then run the BayesianOptimizer for 50 iterations
+    # Then run the BayesianOptimizer for 40 iterations
     optimizer = gpflowopt.BayesianOptimizer(domain, hvpoi, optimizer=acquisition_opt, verbose=True)
-    result = optimizer.optimize(objective_func, n_iter=20)
-
-    #print(result)
+    optimizer.optimize(objective_func, n_iter=30)
 
     pf, dom = gpflowopt.pareto.non_dominated_sort(hvpoi.data[1])
-    print ("::::::::::::::::::::::::::::::::::::::::::::::::::")
-    print (hvpoi.data[1][:,0])
-    print ("##########################")
-    print (hvpoi.data[1][:,1])
 
     plt.scatter(hvpoi.data[1][:,0], hvpoi.data[1][:,1], c=dom)
     plt.title('Pareto set')
@@ -177,9 +170,15 @@ def baysian_opt():
     plt.show()
 
     plt.plot(np.arange(0, hvpoi.data[0].shape[0]),np.minimum.accumulate(hvpoi.data[1][:,0]) ,'b',label='Inference Time')
+    plt.ylabel('fmin')
+    plt.xlabel('Number of evaluated points')
+    plt.legend()
+    plt.show()
+
     plt.plot(np.arange(0, hvpoi.data[0].shape[0]),np.minimum.accumulate(hvpoi.data[1][:,1]) ,'g',label='Power Consumption')
     plt.ylabel('fmin')
     plt.xlabel('Number of evaluated points')
     plt.legend()
     plt.show()
 baysian_opt()
+random_search()
